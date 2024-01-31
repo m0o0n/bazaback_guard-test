60da44853b7e8bd359b63b90f33c0c582bf2ff21
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-01-31 16:02:14 +0200

    feat: add response body example for partners CRUD endpoint in swagger documentation

--- src/partners/dto/create-partner.dto.ts
+++ src/partners/dto/create-partner.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreatePartnerDto {
+  @ApiProperty()
   @IsString()
   name: string;
-
+  
+  @ApiProperty()
   @IsString()
   partner_url: string;
 }
