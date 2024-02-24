919532dc188ad73e78c36d0f51f71c913a27eabe
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-02-24 11:46:17 +0200

    fix: remove console log

--- src/role-guard.ts
+++ src/role-guard.ts
@@ -1,16 +1,12 @@
 
-import { CanActivate, ExecutionContext, Injectable, Request } from '@nestjs/common';
-// import { UserService } from './user/user.service';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-    // constructor(private readonly userService: UserService){}
- 
     async canActivate(context: ExecutionContext) {
         const req = context.switchToHttp().getRequest()
-        // const user = await this.userService.findOne(req.user.email)  
         const user = req.user
-        console.log(req.user)
         if(user.role === 'admin'){
              return true
         }
