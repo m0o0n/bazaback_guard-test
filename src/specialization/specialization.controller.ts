083689b1bbdabfce81fa69b98bffbd5b6cb80109
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-02-27 17:55:53 +0200

    change path import

--- src/specialization/specialization.controller.ts
+++ src/specialization/specialization.controller.ts
@@ -1,4 +1,4 @@
-// import { RoleGuard } from './../auth/role/role.guard';
+import { RoleGuard } from './../auth/role/role.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Specialization } from 'src/specialization/entities/specialization.entity';
 import {
@@ -17,7 +17,6 @@ import { UpdateSpecializationDto } from './dto/update-specialization.dto';
 import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ISpecializationWithStack } from './types';
 import { NotFoundResponse } from '../types';
-import { RoleGuard } from 'src/auth/role/role.guard';
 
 @ApiTags('Specializations')
 @Controller('specialization')
@@ -32,7 +31,7 @@ export class SpecializationController {
     type: CreateSpecializationDto,
   })
   @Post()
-  @UseGuards(RoleGuard, JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RoleGuard)
   create(@Body() createSpecializationDto: CreateSpecializationDto) {
     return this.specializationService.create(createSpecializationDto);
   }
